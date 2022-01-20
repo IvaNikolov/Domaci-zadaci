3f882d869bbe68875fe392b24138a6a6045e6a40
IvaNikolov <ivanikolov86@gmail.com>
2022-01-20 12:21:54 +0100

    Ispit

--- src/d_18_01_2022/Ispit.java
+++ src/d_18_01_2022/Ispit.java
@@ -10,42 +10,52 @@ public class Ispit {
 //	metodu koja kaze da li je ispit polozen (polozen je ako je ocena od 6 do 10)
 //	metodu koja stampa ispit u formatu:
 //	(naziv predmeta) - (profesor) - (ocena)
-private String naziv;
-private int ocena;
-private String imePrezimeProf;
-public Ispit(String naziv, int ocena, String imePrezimeProf) {
-	super();
-	this.naziv = naziv;
-	this.ocena = ocena;
-	this.imePrezimeProf = imePrezimeProf;
-}
-public String getNaziv() {
-	return naziv;
-}
-public void setNaziv(String naziv) {
-	this.naziv = naziv;
-}
-public int getOcena() {
-	return ocena;
-}
-public void setOcena(int ocena) {
-	this.ocena = ocena;
-}
-public String getImePrezimeProf() {
-	return imePrezimeProf;
-}
-public void setImePrezimeProf(String imePrezimeProf) {
-	this.imePrezimeProf = imePrezimeProf;
-}
+	private String naziv;
+	private int ocena;
+	private String imePrezimeProf;
+
+	public Ispit(String naziv, int ocena, String imePrezimeProf) {
+		super();
+		this.naziv = naziv;
+		this.ocena = ocena;
+		this.imePrezimeProf = imePrezimeProf;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public int getOcena() {
+		return ocena;
+	}
+
+	public void setOcena(int ocena) {
+		this.ocena = ocena;
+	}
+
+	public String getImePrezimeProf() {
+		return imePrezimeProf;
+	}
+
+	public void setImePrezimeProf(String imePrezimeProf) {
+		this.imePrezimeProf = imePrezimeProf;
+	}
+
 //metodu koja kaze da li je ispit polozen (polozen je ako je ocena od 6 do 10)
-public boolean polozenIspit() {
-	boolean polozenIspit=false;
-	if(this.ocena>5 && this.ocena<11) {
-		polozenIspit=true;
-	}return polozenIspit;
-}
+	public boolean polozenIspit() {
+		boolean polozenIspit = false;
+		if (this.ocena > 5 && this.ocena < 11) {
+			polozenIspit = true;
+		}
+		return polozenIspit;
+	}
+
 //(naziv predmeta) - (profesor) - (ocena)
-public void print() {
-	System.out.println(this.naziv+" - "+this.imePrezimeProf+" - "+this.ocena);
-}
+	public void print() {
+		System.out.println(this.naziv + " - " + this.imePrezimeProf + " - " + this.ocena);
+	}
 }
